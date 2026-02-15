5b29eea2b6dd3d38123c4fc654865c3cbb8f31f4
DaanV2 <2393905+DaanV2@users.noreply.github.com>
2026-02-15 12:02:43 +0100

    chore: adding documentation

--- interfaces.go
+++ interfaces.go
@@ -3,10 +3,10 @@ package bloomfilters
 // IBloomFilter defines the interface for bloom filter implementations.
 // It provides methods for adding elements, testing membership, and managing hash values.
 type IBloomFilter interface {
-	SetHash(hash uint64)
-	GetHash(hash uint64) bool
-	Add(data []byte)
-	Test(data []byte) bool
-	BitsCount() uint64
-	Bits() Bits
+	SetHash(hash uint64)      // SetHash sets the bit corresponding to the given hash value in the bloom filter.
+	GetHash(hash uint64) bool // GetHash checks if the bit corresponding to the given hash value is set in the bloom filter.
+	Add(data []byte)          // Add adds the given data to the bloom filter by applying the hash functions and setting the corresponding bits.
+	Test(data []byte) bool    // Test checks if the given data is likely to be in the bloom filter by applying the hash functions and checking the corresponding bits.
+	BitsCount() uint64        // BitsCount returns the total number of bits that are set to 1 in the bloom filter.
+	Bits() Bits               // Bits returns the underlying bit array of the bloom filter.
 }
